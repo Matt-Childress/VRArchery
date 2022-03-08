cc59ffa1afd46b1fe0ef0df7fdd2a032997623e6
Matt Childress <mattchildressgamedev@gmail.com>
2022-03-08 00:43:29 -0600

    add some documentation to bowstring

--- Assets/Scripts/Bowstring.cs
+++ Assets/Scripts/Bowstring.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Bowstring : MonoBehaviour
 {
+    //this should be where the two halves of the string meet and create an angle
     public Transform nockPoint;
 
+    //this half of the string's line renderer
     private LineRenderer lineRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        //assign linerenderer on start
         lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //each frame, update the endpoint position to the nockpoint to track a string pull
         if (lineRenderer.positionCount > 1)
         {
             lineRenderer.SetPosition(1, transform.InverseTransformPoint(nockPoint.position));
