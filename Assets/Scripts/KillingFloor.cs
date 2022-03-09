e74d9f1af9a4a66854e92e0714f2b97d7ffa006f
Matt Childress <mattchildressgamedev@gmail.com>
2022-03-09 12:37:42 -0600

    Handling if an arrow or bow gets dropped off the edge of the map
    -arrows get destroyed
    -bow gets placed at starting position

---
+++ Assets/Scripts/KillingFloor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingFloor : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag.Equals("Arrow"))
+        {
+            //destroy dropped arrows
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+            //place dropped bow at starting position
+            collision.transform.position = Vector3.up;
+        }
+    }
+}
