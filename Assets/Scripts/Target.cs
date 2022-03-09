9ab88357ff810666daa1c73b3c113b6828daf303
Matt Childress <mattchildressgamedev@gmail.com>
2022-03-08 20:10:07 -0600

    Score displayed in UI text

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
+    //the text to display hits on
+    public Text scoreText;
+
+    //track the number of hits
+    private int hits;
+
     private void OnCollisionEnter(Collision collision)
     {
         GameObject gO = collision.gameObject;
@@ -9,6 +16,16 @@ public class Target : MonoBehaviour
         {
             //when an arrow collides with the target, freeze it by setting the arrow to kinematic
             gO.GetComponent<Arrow>().rB.isKinematic = true;
+
+            //increment hit count
+            hits++;
+            UpdateScoreText();
         }
+    }
+
+    private void UpdateScoreText()
+    {
+        //display hits
+        scoreText.text = "Hits: " + hits.ToString();
     }
 }
